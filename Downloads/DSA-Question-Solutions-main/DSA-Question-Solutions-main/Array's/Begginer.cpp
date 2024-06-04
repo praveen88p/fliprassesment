920c96c15867df591b29fa9da0ae934e00ebcbfb
praveen88p <praveenpatidarpatidar88@gmail.com>
2024-06-04 21:37:31 +0530

    Commit #4

--- Downloads/DSA-Question-Solutions-main/DSA-Question-Solutions-main/Array's/Begginer.cpp
+++ Downloads/DSA-Question-Solutions-main/DSA-Question-Solutions-main/Array's/Begginer.cpp
@@ -1,9 +1,69 @@
 
 Que = Remove duplicate from sorted Array
 LINK = https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int j=1;
+        for(int i = 0;i<nums.size()-1;i++){
+          if(nums[i]!=nums[i+1]){
+            nums[j]=nums[i+1];
+            j++;
+          }
+        }
+        return j;
+    }
+};
+
+QUE = Second largest 
+LINK = https://www.geeksforgeeks.org/problems/second-largest3735/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=second-largest
+
+class Solution{
+public:	
+	// Function returns the second
+	// largest elements
+	int print2largest(int arr[], int n) {
+	    // code here'
+	    
+	   
+	  int largest = arr[0];
+	  int slargest =-1;
+	  
+	  
+	  if(n<=1){
+	      return -1;
+	  }
+	  
+	  for(int i=1;i<n;i++){
+	      if(arr[i]>largest){
+	          slargest = largest;
+	          largest = arr[i];
+	      }
+	      else if(arr[i]<largest && arr[i]>slargest){
+	          slargest = arr[i];
+	      }
+	  }
+	  return slargest;
+	}
+};
 
 
-
+QUE = LARGEST ELEMENT
+LINK= https://www.geeksforgeeks.org/problems/largest-element-in-array4009/0?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=largest-element-in-array    
+class Solution
+{
+public:
+    int largest(vector<int> &arr, int n)
+    {
+        int ans = INT_MIN;
+        for(int i =0;i<n;i++){
+            if(arr[i]>ans){
+                ans = arr[i];
+            }
+        }
+        return ans;
+    }
+};
 
 
 
