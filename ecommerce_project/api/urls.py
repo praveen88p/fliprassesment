57b09af8fef12b9341c0a7d40e585039c306a9d8
Praveen Patidar <102358171+praveen88p@users.noreply.github.com>
2024-11-09 04:36:26 +0530

    Update urls.py

--- ecommerce_project/api/urls.py
+++ ecommerce_project/api/urls.py
@@ -9,5 +9,16 @@ urlpatterns = [
   path('updateproduct/<int:productId>/', views.update_product, name='update_product'),
       path('deleteproduct/<int:productId>/', views.delete_product, name='delete_product'),
         path('cart/add', views.add_to_cart, name='add_to_cart'),
+    path('cart/update', views.update_cart, name='update_cart'),
+
+    path('cart/delete', views.delete_cart_item, name='delete_cart_item'),
+    path('cart', views.get_cart_details, name='get_cart_details'),
+    path('placeorder', views.place_order, name='place_order'),
+    path('getallorders', views.get_all_orders, name='get_all_orders'),
+    path('orders/customer/<int:customerId>', views.get_orders_by_customer, name='get_orders_by_customer'),
+
+
+
+
 
 ]
