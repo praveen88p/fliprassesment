5dd0c0cc8b3e9e0e0f43894e9745425134552b07
Praveen Patidar <102358171+praveen88p@users.noreply.github.com>
2024-11-09 01:12:16 +0530

    Add signin url or other

--- ecommerce_project/api/urls.py
+++ ecommerce_project/api/urls.py
@@ -1,9 +1,13 @@
-# api/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('products/', views.list_products, name='list_products'),
     path('addproduct', views.add_product, name='add_product'),
-    # other routes for sign up, sign in, cart management, etc.
+  path('signup/', views.signup, name='signup'),
+  path('signin/', views.signin, name='signin'),
+  path('updateproduct/<int:productId>/', views.update_product, name='update_product'),
+      path('deleteproduct/<int:productId>/', views.delete_product, name='delete_product'),
+        path('cart/add', views.add_to_cart, name='add_to_cart'),
+
 ]
